43a6b5bf57d1ff88adf8adb1b9b2b9a5e7896337
“Torikul <torikul@agate.id>
2021-11-23 18:13:36 +0700

    initial commit

---
+++ assets/js/dummyData.js
@@ -0,0 +1,32 @@
+const articlesData = [
+    {
+        id: "1",
+        title: "Mainkan Fantasy team, pria ini dapat pulsa 100Juta",
+        desc : "lorem lorem lorem",
+        imgCode : "banner1"
+    },
+    {
+        id: "2",
+        title: "Game Fantasy Football Liga 1 hadir di Vidio",
+        desc : "lorem lorem lorem",
+        imgCode : "banner2"
+    },
+    {
+        id: "3",
+        title: "Cuman modal jempol, dapat pulsa gratis",
+        desc : "lorem lorem lorem",
+        imgCode : "banner3"
+    },
+    {
+        id: "4",
+        title: "Yuk mainkan super fantasy",
+        desc : "lorem lorem lorem",
+        imgCode : "banner4"
+    },
+    {
+        id: "5",
+        title: "Asiik fantasy team game terbaik pilihan masyarakat",
+        desc : "lorem lore lorem",
+        imgCode : "banner5"
+    },
+]
